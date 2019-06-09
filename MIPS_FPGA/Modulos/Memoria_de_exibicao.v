4a28e67e964d776010b63f183794c41972988e8f
Samuel <samuelpedro1999@gmail.com>
2019-06-08 22:58:52 -0300

    Corrigido bug

--- MIPS_FPGA/Modulos/Memoria_de_exibicao.v
+++ MIPS_FPGA/Modulos/Memoria_de_exibicao.v
@@ -1,15 +1,18 @@
-module Mem_Exibi(resultadoALU,Clk,proximo,saida);
+module Mem_Exibi(resultadoALU,Clk,proximo,saida,reset);
   input [31:0] resultadoALU;
-  input Clk,proximo;
+  input Clk,proximo,reset;
   output reg [31:0]saida;
   reg [31:0]Mem[0:25];
-  integer i = 0;
+  integer i = 26;
   integer j = 0;
 
   always @ ( negedge Clk ) begin
     if(i<25) begin
-    Mem[i] <= resultadoALU;
-    i <= i + 1;
+      Mem[i] <= resultadoALU;
+      i <= i + 1;
+    end
+    else if(reset) begin
+      i<=0;
     end
   end
 
